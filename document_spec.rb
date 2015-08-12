b264aeb59a96734a061ef7a9da4d9f2b853d908b
Aaron Addleman <aaronaddleman@gmail.com>
2015-08-11 17:18:26 -0700

    add basic example with simple test
    the only test we have right now is for counting the words in the content

---
+++ document_spec.rb
@@ -0,0 +1,17 @@
+require_relative 'document'
+
+describe "document" do
+  before(:all) do
+    
+    @full_doc = Document.new("Lego Land Adventures!", "Aaron Addleman", "Lego Land is a great place for people of all ages, well, actually you need to be 3 or older. Nah, never mind, its best for everyone.")
+    
+  end
+
+  describe "a simple document" do
+    
+    it "should open count the words" do
+      expect(@full_doc.word_count).to eq(27)
+    end
+
+  end
+end
